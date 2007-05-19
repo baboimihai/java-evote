3a8e4bb9cf8f3e64accb7f8f9aed749ae81c5e4b
marianobesio <marianobesio@930e114f-842f-0410-a017-b5c97c80b7d2>
2007-05-19 05:32:53 +0000

    Modificada api de encriptador, desencriptador, firmador y validador

--- src/Firmador.java
+++ src/Firmador.java
@@ -1,18 +1,27 @@
-/**
- *
- */
+import java.util.List;
 
 /**
- * @author be
- *
+ * Esta clase implementa el firmador. Se utiliza para firmar mensajes con una
+ * clave dada.
  */
 public class Firmador {
 
-	public Firmador (String key) {
+	/**
+	 * Construye un nuevo firmador a partir de una clave privada
+	 * @param key clave a usar para el firmador
+	 * @throws Exception si la clave es inválida
+	 */
+	public Firmador (String key) throws Exception {
 		return;
 	}
 
-	public String firmar (String mensaje) {
+
+	/**
+	 * Firma una lista de mensajes
+	 * @param messageList lista de mensajes a firmar
+	 * @return un String con todos los mensajes firmados
+	 */
+	public String firmar (List<String> messageList) {
 		return "MENSAJE FIRMADO";
 	}
 
