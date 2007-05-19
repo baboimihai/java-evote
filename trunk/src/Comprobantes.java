9a9914c92428353bf56ff2e964f3919905579d99
hrajchert <hrajchert@930e114f-842f-0410-a017-b5c97c80b7d2>
2007-05-19 04:00:36 +0000

    Cambios en la API de comprobantes para aceptar varias votaciones

--- trunk/src/Comprobantes.java
+++ trunk/src/Comprobantes.java
@@ -3,7 +3,7 @@
  * 
  */
 
-// TODO Spock, tambien tenes que implementar la interfaz Iterable.
+// TODO Spock, tambien tenes que implementar la interfaz "Iterable".
 // La interfaz Iterator o la implementas aca o haces otra clase que la implemente...
 // En otras palabras voy a tener que poder iterar.
 import java.util.*;
@@ -19,18 +19,28 @@ public class Comprobantes {
 		return false;
 	}
 	/**
-	 * Devuelve el comprobante en forma de lista.
+	 * Devuelve el comprobantes como listas.
 	 * Tira Excepcion si no está.
 	 * @param uvi Clave publica del iesimo votante
 	 * @return la lista formada por (usvu, tokenFirmado).
 	 */
-	public List obtenerComprobante(String uvi) throws Exception {
+	public List obtenerComprobante(String uvi, String idv) throws Exception {
+		//TODO: Internamente tenes que buscar en la base todos los token que tengan 
+		// el uvi, desencriptarlos (con el uvi) y fijarte si el id es el que te mando
+		// yo.
 		return null;
 	}
-	
 	/**
 	 * Marca a un votante como que ya votó
 	 * @param usvu Secreto compartido entre el votante y la urna encriptado con Uu
 	 */
 	public void marcarVotado(String usvu) throws Exception {}
+	
+	/**
+	 * Devuelve si está marcado como que votó o no.
+	 */
+	public boolean yaVoto(String usvu) throws Exception {
+		return true;
+	}
+
 }
