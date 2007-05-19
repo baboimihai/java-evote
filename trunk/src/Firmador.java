f5e6724a4b9df29de77339ae9952f35fa9db1251
marianobesio <marianobesio@930e114f-842f-0410-a017-b5c97c80b7d2>
2007-05-19 04:06:03 +0000

    Agrego mi api Faltan las execptions

---
+++ trunk/src/Firmador.java
@@ -0,0 +1,20 @@
+/**
+ *
+ */
+
+/**
+ * @author be
+ *
+ */
+public class Firmador {
+
+	public Firmador (String key) {
+		return;
+	}
+
+	public String firmar (String mensaje) {
+		return "MENSAJE FIRMADO";
+	}
+
+
+}
