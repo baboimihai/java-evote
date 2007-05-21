8087a3c9592d20ed386b29170c4268ce54881a79
hrajchert <hrajchert@930e114f-842f-0410-a017-b5c97c80b7d2>
2007-05-21 06:46:21 +0000

    Otra vez eleccion

---
+++ trunk/src/Eleccion/ComprobanteNotFoundException.java
@@ -0,0 +1,44 @@
+package eleccion;
+/**
+ * 
+ */
+
+/**
+ * @author carranca
+ *
+ */
+public class ComprobanteNotFoundException extends Exception {
+
+	/**
+	 * 
+	 */
+	public ComprobanteNotFoundException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param message
+	 */
+	public ComprobanteNotFoundException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param cause
+	 */
+	public ComprobanteNotFoundException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param message
+	 * @param cause
+	 */
+	public ComprobanteNotFoundException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+}
