e9b1cf4f783d3083b129781467b3f2a0baadb1a8
pabloggarcia <pabloggarcia@930e114f-842f-0410-a017-b5c97c80b7d2>
2007-05-21 05:21:23 +0000

    Excepción por votante inválido.

---
+++ src/VotanteInvalidoException.java
@@ -0,0 +1,41 @@
+
+/**
+ * @author pagarcia
+ *
+ */
+public class VotanteInvalidoException extends Exception
+{
+	/**
+	 * 
+	 */
+	public VotanteInvalidoException()
+	{
+
+	}
+
+	/**
+	 * @param message
+	 */
+	public VotanteInvalidoException(String message)
+	{
+		super(message);
+	}
+
+	/**
+	 * @param cause
+	 */
+	public VotanteInvalidoException(Throwable cause)
+	{
+		super(cause);
+	}
+
+	/**
+	 * @param message
+	 * @param cause
+	 */
+	public VotanteInvalidoException(String message, Throwable cause)
+	{
+		super(message, cause);
+	}
+
+}
