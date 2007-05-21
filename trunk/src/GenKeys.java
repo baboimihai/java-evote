545b2d051c548aba5799cbf7c4cf60a3993e39e0
hrajchert <hrajchert@930e114f-842f-0410-a017-b5c97c80b7d2>
2007-05-21 02:15:03 +0000

    cambie GenKeys para que sea un miniprog

--- trunk/src/GenKeys.java
+++ trunk/src/GenKeys.java
@@ -1,3 +1,4 @@
+import java.io.*;
 import java.math.BigInteger;
 import java.security.SecureRandom;
 
@@ -10,12 +11,12 @@ public class GenKeys {
 	 * bitsLargo largo de la clave, si es 0 => default 1024
 	 * @return 1ro la pública y después la privada
 	 */
-	public static String[] generakeys (int bitsLargo) {
+	public static void main (String [] args) throws Exception {
 
+		int bitsLargo = new Integer(args[0]);
 		if (bitsLargo == 0)
 			bitsLargo = 1024;
-
-		String[] pair = new String[2];
+		
 		BigInteger one      = new BigInteger("1");
 		SecureRandom random = new SecureRandom();
 
@@ -26,11 +27,21 @@ public class GenKeys {
 		BigInteger publicKeyN = new BigInteger("65537");     // common value in practice = 2^16 + 1
 		BigInteger privateKeyN = publicKeyN.modInverse(phi);
 		BigInteger modulus = p.multiply(q);
+		
 
-		pair[0] = new String("public = " + publicKeyN + "\nmodulus = " + modulus);
-		pair[1] = new String("private = " + privateKeyN + "\nmodulus = " + modulus);
+		DataOutputStream priv = new DataOutputStream(
+				 new BufferedOutputStream( new FileOutputStream("privada.key")));
+		DataOutputStream pub = new DataOutputStream(
+				 new BufferedOutputStream( new FileOutputStream("publica.key")));
 
-		return pair;
+		String publica = new String("public = " + publicKeyN + "\nmodulus = " + modulus);
+		String privada = new String("private = " + privateKeyN + "\nmodulus = " + modulus);
+		System.out.println(publica);
+		System.out.println(privada);
+		pub.writeChars(publica);
+		priv.writeChars(privada);
+		pub.close();
+		priv.close();
 	}
 }
 
