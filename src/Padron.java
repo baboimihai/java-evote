27b51bdafc3d6ec78fa1f3a2dc49b43836995b4c
ezequiel85 <ezequiel85@930e114f-842f-0410-a017-b5c97c80b7d2>
2007-05-21 03:42:46 +0000

    Mas o menos lo probe y anduvo :p

--- src/Padron.java
+++ src/Padron.java
@@ -1,19 +1,20 @@
 /**
  * 
- * @author ebalaguer
- * @version 0.1a
- * La clase Padron bla bla bla
+ * @author ezequiel85
+ * @version 1.0
+ * La clase Padron hace que Ingui se la coma mas rapido
  *
  */
 import java.io.*;
-import java.sql.*;
 import java.util.*;
 
 public class Padron
-{
-	private BufferedReader br;
+{	
+	private BufferedReader votantes, votaciones;
 	private String line;
 	private String group = null;
+	private Hashtable<String, Votante> votante_list;
+	private Hashtable<String, Votacion> votacion_list;
 	private static Padron ref;
 	
 	/**
@@ -21,7 +22,8 @@ public class Padron
 	 */
 	private Padron()
 	{
-		// TODO: Inicializaciones
+		votante_list = new Hashtable<String, Votante>();
+		votacion_list = new Hashtable<String, Votacion>();
 	}
 			
 	/**
@@ -34,6 +36,70 @@ public class Padron
 			ref = new Padron();
 		return ref;
 	}
+	
+	/** Metodo privado para cargar votantes (solo para ordenar un poco cargarPadron)
+	 * @param void
+	 * @throws IOException
+	 */
+	private void cargarVotaciones() throws IOException
+	{
+		String nombre = null;
+		while ((line = this.votaciones.readLine()) != null)
+		{
+			if (line.charAt(0) == '0')
+			{
+				nombre = (line.split(","))[1];
+				votacion_list.put(nombre, new Votacion(nombre));
+				continue;
+			}
+			String[] splitted;
+			Votacion v;
+			splitted = line.split(",");
+			
+			if (line.charAt(0) == '1') //Nueva lista
+			{
+				v = votacion_list.get(nombre);
+				v.addOpcion(splitted[1], splitted[2]);
+				votacion_list.put(nombre, v);
+			}
+			else // Nuevo grupo que participa en la votacion
+			{
+				v = votacion_list.get(nombre);
+				v.addGrupo(splitted[1]);
+				votacion_list.put(nombre, v);
+			}
+		}		
+	}
+	
+	/** Metodo privado para cargar votantes (solo para ordenar un poco cargarPadron)
+	 * @param void
+	 * @throws IOException
+	 */
+	private void cargarVotantes() throws IOException
+	{
+		while ((line = this.votantes.readLine()) != null)
+		{
+			if (line.charAt(0) == '0')
+			{
+				group = (line.split(","))[1];
+				continue;
+			}
+			String[] splitted;
+			splitted = line.split(",");
+			
+			if (votante_list.containsKey(splitted[2]))
+			//Si el hash ya contiene a este DNI, es porque esta en mas de un grupo
+			{
+				Votante v = votante_list.get(splitted[2]);
+				v.addGrupo(group);
+				votante_list.put(splitted[2], v);
+			}
+			else // Si no, insertamos un nuevo votante
+			{
+				votante_list.put(splitted[2], new Votante(splitted[2],splitted[1], splitted[3], group));
+			}
+		}		
+	}
 
 	/**
 	 * Carga en memoria el padron
@@ -43,16 +109,11 @@ public class Padron
 	 */
 	public void cargarPadron(String votantes, String votaciones) throws IOException
 	{
-		br = new BufferedReader(new FileReader(votantes));
+		this.votantes = new BufferedReader(new FileReader(votantes));
+		this.votaciones = new BufferedReader(new FileReader(votaciones));
 
-		while ((line = br.readLine()) != null)
-		{
-			if (line.charAt(0) == '0')
-			{
-				group = line.substring(3);
-			}
-			
-		}
+		this.cargarVotantes();
+		this.cargarVotaciones();
 	}
 
 	/**
@@ -64,57 +125,217 @@ public class Padron
 	  }
 	
 	/**
-	 * Dado un DNI y un ID de votación consulta si el votante puede votar en dicha elección
+	 * Dado un DNI y un ID de votación consulta si el votante puede votar en dicha eleccion
 	 * @param dni String con el DNI
-	 * @param idv ID de la votación
-	 * @throws Exception Si no se encontró el par (dni, idv)
+	 * @param idv ID de la votacion
+	 * @throws Exception Si no se encontro el par (dni, idv)
 	 */
 	public boolean puedeVotar(String dni, String idv) throws Exception
 	{
+		Votante v;
+		Votacion vo;
+		Iterator i;
+		String grupo;
+		
+		v = votante_list.get(dni);
+		if (v == null)
+			throw new Exception("El DNI no corresponde a ningun votante");
+		
+		vo = votacion_list.get(idv);
+		if (vo == null)
+			throw new Exception("La votacion solicitada no existe");
+		
+		i = vo.grupos.iterator();
+		while (i.hasNext())
+		{
+			grupo = (String)i.next();
+			if (v.grupo.contains(grupo))
+				return true;
+		}
 		return false;
 	}
 	
 	/**
 	 * Dado un DNI devuelve una lista con las votaciones en que puede participar el votante
 	 * @param dni String con el DNI
-	 * @throws Exception Si no se encontró el dni
+	 * @throws Exception Si no se encontro el dni
 	 */
 	public List<String> getVotaciones(String dni) throws Exception
 	{
-		return null;
+		Votacion vo;
+		Votante v;
+		Iterator i;
+		List<String> a;
+		Enumeration<Votacion> e;
+		
+		a = new ArrayList<String>();
+		
+		v = votante_list.get(dni);
+		if (v == null)
+			throw new Exception("El DNI no corresponde a ningun votante");
+		
+		i = v.grupo.iterator();
+		
+		while(i.hasNext())
+		{
+			String rec;
+			
+			e = votacion_list.elements();
+			rec = (String) i.next();
+			while(e.hasMoreElements())
+			{	
+				vo = e.nextElement();
+				for(String grupoparticipante:vo.grupos)
+				{
+					if (grupoparticipante.equals(rec) && !a.contains(vo.nombre))
+					{
+						a.add(vo.nombre);
+					}
+				}		
+			}
+		}
+		return a;
 	}
 	
 	/**
 	 * Devuelve las opciones para un idv
-	 * @param idv El id de la votación
+	 * @param idv El id de la votacion
 	 * @return La lista de Strings con las opciones
-	 * @trows Exception Si no existía la votacion
+	 * @throws Exception Si no existia la votacion
 	*/
 	public List<String> getOpciones(String idv) throws Exception
 	{
-		return Arrays.asList("a", "b");
+		Votacion vo;
+		List<String> l;
+		Enumeration<String> e;
+		
+		l = new ArrayList<String>();
+		vo = votacion_list.get(idv);
+		if (vo == null)
+			throw new Exception("La votacion solicitada no existe");
+		e = getUOpc(vo.nombre).keys();
+		while(e.hasMoreElements())
+		{
+			l.add(e.nextElement());
+		}
+		
+		return l;
 	}
 	
 	/**
-	 * Dado un DNI devuelve la clave pública asociada
-	 * @param dni String con el DNI
-	 * @throws Exception Si no se encontró el dni
-	 */
-	public String getUvi(String dni) throws Exception
-	{
-		return "";
-	}
-	
-	/**
-	 * Dada una clave pública devuelve el DNI correspondiente
-	 * @param uvi String con la clave pública
-	 * @throws Exception Si no se encontró el uvi
+	 * Dada una clave publica devuelve el DNI correspondiente
+	 * @param uvi String con la clave publica
+	 * @throws Exception Si no se encontro el uvi
 	 * 
 	 */
 	public String getDNI(String uvi) throws Exception
 	{
-		return "";
+		Votante v;
+		Enumeration<Votante> e;
+		
+		e = votante_list.elements();
+		while(e.hasMoreElements())
+		{
+			v = e.nextElement();
+			if (v.getUvi().equals(uvi))
+				return v.getDNI();
+		}
+		throw new Exception("El votante indicado no existe");
+		//Si llego aca es que la uvi indicada no existe
+	}
+
+	/**
+	 * Dado un DNI devuelve la clave publica asociada
+	 * @param dni String con el DNI
+	 * @throws Exception Si no se encontro el dni
+	 */
+	public String getUvi(String dni) throws Exception
+	{
+		Votante v;
+		
+		v = votante_list.get(dni);
+		if (v == null)
+			throw new Exception("El votante indicado no existe");
+		
+		return v.getUvi();
 	}
 	
-
+	/**
+	 * Dado un ID de votacion retorna un hash con los pares (opcion, clave pública)
+	 * @param idv
+	 * @throws Exception Si no se encontro la votacion
+	 */
+	public Hashtable<String,String> getUOpc(String idv) throws Exception
+	{
+		Votacion vo;
+		
+		vo = votacion_list.get(idv);
+		if (vo == null)
+			throw new Exception("La votacion solicitada no existe");
+		return vo.getListas();
+	}
+	
+	private class Votacion
+	{
+		private String nombre;
+		private Hashtable<String,String> listas;
+		private ArrayList<String> grupos;
+		
+		public Votacion(String nombre)
+		{
+			this.nombre = nombre;
+			this.listas = new Hashtable<String,String>();
+			this.grupos = new ArrayList<String>();
+		}
+		
+		public void addOpcion(String candidato, String uvi)
+		{
+			this.listas.put(candidato, uvi);
+		}
+		
+		public void addGrupo(String grupo)
+		{
+			this.grupos.add(grupo);
+		}
+		
+		public Hashtable<String, String> getListas()
+		{
+			return this.listas;
+		}
+	}
+	
+	private class Votante
+	{
+		@SuppressWarnings("unused")
+		private String dni;
+		@SuppressWarnings("unused")
+		private String nombre;
+		@SuppressWarnings("unused")
+		private String uvi;
+		private ArrayList<String> grupo;
+		
+		public Votante(String dni, String nombre, String uvi, String grupo)
+		{
+			this.grupo = new ArrayList<String>();
+			this.dni = dni;
+			this.nombre = nombre;
+			this.uvi = uvi;
+			this.grupo.add(grupo);
+		}
+		
+		public void addGrupo(String grupo)
+		{
+			this.grupo.add(grupo);
+		}
+		
+		public String getDNI()
+		{
+			return this.dni;
+		}
+		
+		public String getUvi()
+		{
+			return this.uvi;
+		}
+	}
 }
