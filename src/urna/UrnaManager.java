2c77f69c8185f69ba3451bc3a60770e740e898ec
hrajchert <hrajchert@930e114f-842f-0410-a017-b5c97c80b7d2>
2007-05-24 03:58:04 +0000

    Correjidos un par de warnings

--- src/urna/UrnaManager.java
+++ src/urna/UrnaManager.java
@@ -3,7 +3,7 @@ import java.util.*;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
-import java.net.*;
+
 
 import eleccion.ComprobantesUrna;
 
