ff54ec2a4efc1e293a5a68bf07036aefbcfefc13
marianobesio <marianobesio@930e114f-842f-0410-a017-b5c97c80b7d2>
2007-06-04 05:36:19 +0000

    GenPadrón volvió a la normalidad

--- trunk/src/eleccion/GenPadron.java
+++ trunk/src/eleccion/GenPadron.java
@@ -5,46 +5,46 @@ package eleccion;
  */
 
 import java.io.*;
-//import java.util.Hashtable;
 
 import criptografia.GenKeys;
 import eleccion.InfoServidores;
 
 public class GenPadron
 {
-	
+
 	private BufferedReader r_votante;
 	private BufferedReader r_votacion;
 	private BufferedWriter w_votante;
 	private BufferedWriter w_votacion;
-	long dni = 30240000;
-	
+//	long dni = 30240000;
+
 	/**
-	 * 
+	 *
 	 * @param args, primer parametro archivo de votantes, segundo de votaciones
-	 * NO VALIDO NADA, asi que si esta mal, AGUA Y AJO
+	 * NO VALIDO NADA, asi que si esta mal, AJO Y AGUA
 	 */
 	public static void main(String[] args)
 	{
 		@SuppressWarnings("unused")
 		GenPadron p;
-		
+
 		try
 		{
-			p = new GenPadron("C:/Documents and Settings/Ezequ/workspace/Evote/src/roles.txt", "C:/Documents and Settings/Ezequ/workspace/Evote/src/votaciones.txt");
+			p = new GenPadron(args[0], args[1]);
+			System.out.println("fin generación padrón");
 		} catch (Exception e)
 		{
 			e.printStackTrace();
 		}
 	}
-	
+
 	public GenPadron(String votante, String votacion) throws Exception
 	{
 		this.r_votante = new BufferedReader(new FileReader(votante));
 		this.r_votacion = new BufferedReader(new FileReader(votacion));
-		this.w_votante = new BufferedWriter(new FileWriter("votante_parsed.txt"));
-		this.w_votacion = new BufferedWriter(new FileWriter("votacion_parsed.txt"));
-		
+		this.w_votante = new BufferedWriter(new FileWriter(InfoServidores.resources + "votantes/votante_parsed.txt"));
+		this.w_votacion = new BufferedWriter(new FileWriter(InfoServidores.resources + "votacion/votacion_parsed.txt"));
+
 		parse_votante(this.r_votante, this.w_votante);
 		parse_votacion(this.r_votacion, this.w_votacion);
 	}
@@ -53,17 +53,17 @@ public class GenPadron
 		String line;
 		String res;
 		String[] splitted;
-		//String dni_string;
-		String path = InfoServidores.readKey("resources") + "votantes/";
-		//String path = "";
+		String dni_string;
+		String path = InfoServidores.resources + "votantes/";
+
 		//String val;
 		//Hashtable<String, String> tablita;
-		
+
 		//tablita = new Hashtable<String, String>();
-		
+
 		while((line = votante.readLine()) != null)
 		{
-			splitted = line.split(",");			
+			splitted = line.split(",");
 			if (line.charAt(0) == '1')
 			{
 				//val = tablita.get(splitted[1]);
@@ -88,37 +88,40 @@ public class GenPadron
 		out.close();
 	}
 
-	void parse_votacion(BufferedReader votante, BufferedWriter out) throws Exception
+	void parse_votacion(BufferedReader votacion, BufferedWriter out) throws Exception
 	{
 		String line;
 		String res;
 		String[] splitted;
-		String path = InfoServidores.readKey("resources") + "votacion/";
+		String path = InfoServidores.resources + "votacion/";
 		//String path = "";
 		int vot_num = 0, opc_num = 0;
-		String vot_num_s, opc_num_s;
-		
-		
-		while((line = votante.readLine()) != null)
+		String idv = null;
+
+
+		while((line = votacion.readLine()) != null)
 		{
 			splitted = line.split(",");
 			if (line.charAt(0) == '0')
 			{
 				vot_num++;
 				opc_num = 0;
+				idv = splitted[1].trim().replace(' ', '_').replace('¿', '_');
 			}
 			else if (line.charAt(0) == '1')
 				opc_num++;
-			
+
 			if (line.charAt(0) == '1')
 			{
-				vot_num_s = (new Long(vot_num)).toString();
-				opc_num_s = (new Long(opc_num)).toString();
-				GenKeys.generarClaves(0,path+ vot_num_s +"_"+ opc_num_s +"_privada.key", path+vot_num_s +"_"+ opc_num_s +"_publica.key");
-				res = splitted[0].trim()+","+splitted[1].trim()+"," + vot_num_s + "_" + opc_num_s;
+				//vot_num_s = (new Long(vot_num)).toString();
+				//opc_num_s = (new Long(opc_num)).toString();
+				String keyName = idv + "_" + splitted[1].trim().replace(' ', '_');
+				GenKeys.generarClaves(0, path + keyName + "_privada.key",
+										 path + keyName + "_publica.key");
+				res = splitted[0].trim() + "," + splitted[1].trim() + "," + keyName;
 			}
 			else
-				res = splitted[0].trim()+","+splitted[1].trim();
+				res = splitted[0].trim() + "," + splitted[1].trim();
 			out.write(res);
 			out.newLine();
 		}
